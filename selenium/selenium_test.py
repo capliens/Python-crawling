374045507a7b7848b3a4282a818e34d79fe6996f
capliens <153248201+capliens@users.noreply.github.com>
2025-05-12 18:06:45 +0900

    공부용 자료

--- selenium/selenium_test.py
+++ selenium/selenium_test.py
@@ -13,3 +13,43 @@ driver.get("https://www.google.com")
 time.sleep(3)
 
 #  정보사이트 https://wikidocs.net/149358
+
+# 밑에껀 사이트를 불러와서 링크를 저장하는 방식 (오류있음)
+                    if link is None:
+                        # original_window = driver.current_window_handle
+                        driver.execute_script(
+                            "arguments[0].click();", link_element)
+                        wait = WebDriverWait(driver, 20)
+                        wait.until(EC.new_window_is_opened(
+                            driver.window_handles))
+                        # 현재 열려있는 모든 창의 핸들을 가져옵니다.
+                        all_windows = driver.window_handles
+                        new_window_handle = None  # 새 창 핸들을 저장할 변수 초기화
+
+                        # 원래 창이 아닌 다른 핸들을 찾습니다.
+                        for window_handle in all_windows:
+                            if window_handle != original_window:
+                                new_window_handle = window_handle
+                                break
+
+                        # 새 창으로 전환합니다.
+                        if new_window_handle:  # 새 창이 열렸는지 확인
+                            driver.switch_to.window(new_window_handle)
+                            # 현재 URL을 가져옵니다.
+                            new_url = driver.current_url
+                            # 새 창을 닫습니다.
+                            driver.close()
+
+                        # 원래 창으로 돌아갑니다.
+                        driver.switch_to.window(original_window)
+
+                except NoSuchElementException:
+                    pass
+                except TimeoutException:
+                    current_windows = driver.window_handles
+                    for window_handle in current_windows:
+                        if window_handle != original_window:
+                            driver.switch_to.window(window_handle)
+                            driver.close()
+                    driver.switch_to.window(original_window)
+                    print("err")
