76f4a84e3c0024a55973ae43408fa60c8105ae7b
capliens <153248201+capliens@users.noreply.github.com>
2025-05-14 16:26:28 +0900

    최적화_전

--- selenium/DB손해보험.py
+++ selenium/DB손해보험.py
@@ -10,66 +10,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver import ActionChains
+import time
 # import sqlite3
 # from datetime import datetime
-
-# -----가져오기----
-
-
-class EpostScraper:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_all_data(self):
-        data_list = []
-        data_list = self._extract(data_list)
-        return data_list
-
-    def _extract(self, data_list):
-        container4 = wait.until(
-            EC.presence_of_element_located((By.ID, "mCSB_4_container")))
-        sale_name = container4.find_element(By.CLASS_NAME, "step4_name")
-        sale_date = container4.find_element(By.CLASS_NAME, "step4_date")
-
-        li_list = wait.until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, "#mCSB_4_container ul.btn_list")))
-        li_elements = li_list.find_elements(By.TAG_NAME, "li")
-
-        for link in li_elements:
-            try:
-                a_tag = link.find_element(By.TAG_NAME, "a")
-                span_tag = a_tag.find_element(By.TAG_NAME, "span")
-
-                href = a_tag.get_attribute("href")
-                text = span_tag.text.strip()
-
-                print(sale_date, sale_name, href, text)
-            except Exception:
-                print("요소 처리 중 오류 발생")
-
-        return data_list
-
 
 # ----- 본문 -------
 options = Options()
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.idbins.com/FWMAIV1534.do")
 wait = WebDriverWait(driver, 10)
+action = ActionChains(driver)
 all_data = []
 
 container = wait.until(
     EC.presence_of_element_located((By.ID, "mCSB_1_container")))
 a_tags = container.find_elements(
-    By.CSS_SELECTOR, "ul > li.dp1 > ul > li > a")
+    By.CSS_SELECTOR, 'ul > li.dp1 a[onclick^="getStep2"]')
 
 for a in a_tags:
     try:
         # 1단계 클릭
-        wait.until(EC.element_to_be_clickable(a)).click()
-
+        driver.execute_script("arguments[0].click();", a)
+        time.sleep(0.3)
         # 2단계 메뉴가 렌더링될 때까지 대기 (step2_list가 등장)
-        wait.until(EC.presence_of_element_located((By.ID, "step2_list")))
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "ul#step2_list > li")))
 
         container2 = driver.find_element(By.ID, "mCSB_2_container")
         a_tag2s = container2.find_elements(
@@ -77,26 +43,41 @@ for a in a_tags:
 
         for b in a_tag2s:
             try:
-                wait.until(EC.element_to_be_clickable(b)).click()
-
+                driver.execute_script("arguments[0].click();", b)
+                time.sleep(0.3)
                 # 3단계 메뉴가 렌더링될 때까지 대기
                 wait.until(EC.presence_of_element_located(
-                    (By.ID, "step3_list")))
+                    (By.CSS_SELECTOR, "ul#step3_list > li")))
 
                 container3 = driver.find_element(By.ID, "mCSB_3_container")
                 a_tag3s = container3.find_elements(
                     By.CSS_SELECTOR, "ul#step3_list > li > a")
-                scroll_value = 0
+
                 for c in a_tag3s:
-                    try:
-                        wait.until(EC.element_to_be_clickable(c)).click()
-                        # 데이터 추출
-                        # scraper2 = EpostScraper(driver)
-                        # all_data.extend(scraper2.get_all_data())
-                        # print("끝내18")
-                    except Exception as e:
-                        print(f"3단계 클릭 오류: {e}")
-                driver.execute_script("window.scrollTo(0, -100);")
+                    # 클릭 실행
+                    driver.execute_script("arguments[0].click();", c)
+                    time.sleep(0.3)
+
+                    wait.until(EC.presence_of_element_located(
+                        (By.CSS_SELECTOR, "ul.result > li")))
+                    container4 = driver.find_element(By.ID, "mCSB_4_container")
+                    name = container4.find_element(
+                        By.ID, "step4_name").text
+                    date = container4.find_element(
+                        By.ID, "step4_date").text
+                    all_data.append(name)
+                    all_data.append(date)
+                    link_elements = container4.find_elements(
+                        By.CSS_SELECTOR, "ul.btn_list > li")
+                    for x in link_elements[:3]:
+                        cell = x.find_element(By.TAG_NAME, "a")
+                        link = cell.get_attribute("onclick").split(
+                            ",")[1].strip().strip('"').strip(")")
+                        text = cell.find_element(By.TAG_NAME, "span").text
+                        http_link = "https://www.idbins.com/cYakgwanDown.do?FilePath=InsProduct/"
+                        full_link = http_link+link
+                        value = (text, full_link)
+                        all_data.append(value)
             except Exception as e:
                 print(f"2단계 클릭 오류: {e}")
 
